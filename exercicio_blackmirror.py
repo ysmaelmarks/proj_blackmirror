37fdab3ad6c026a097110680140239b43bf01859
Ysmael Marques <90981943+ysmaelmarks@users.noreply.github.com>
2024-03-28 08:58:40 -0300

    upload
    upload atividade black mirror

---
+++ exercicio_blackmirror.py
@@ -0,0 +1,20 @@
+while True:
+    pergunta = input("Digite sua pergunta: ")
+    if(pergunta == "QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?"):
+      print(f'Joan Tait')
+    elif(pergunta == "QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?"):
+        print('Um supercomputador')
+    elif(pergunta == "QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?"):
+        print(f'Streamberry')
+    elif(pergunta == "COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?"):
+        print(f'Ela descobre quando percebe que seu dia foi retratado pela série')
+    elif(pergunta == "QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?"):
+        print(f'Ela nega os fatos, mas tende a aceitar e tenta processar a streamberry')
+    elif(pergunta == "QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?"):
+        print(f'Os temas abordados são: LGPD, distopia, inteligencia artificial e realidade simulada')
+    elif(pergunta == "O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE."):
+        print(f'Não, geralmente os finais de blackmirror são dramáticos e tendem a ser distópicos. 
+              O episódio termina de forma agradável quando se entende que a Joan real está
+              feliz por ter saído da realidade virtual da Streamberry')
+    else:
+        break
